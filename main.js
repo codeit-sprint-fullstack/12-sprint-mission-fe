13d25c19c282907791967741b5d509e020e8dbdd
apple <apple@apples-MacBook-Air.local>
2026-03-16 13:30:00 +0900

    fix: product 함수 실행 await으로 처리

--- main.js
+++ main.js
@@ -23,26 +23,49 @@ createArticle("게시글 제목", "내용", "https://이미지url.com/1.jpg").th
   (data) => console.log("생성글", data),
 ); //게시글 생성
 
-patchArticle(5588, { title: "바뀐 제목", content: "바뀐내용" }).then((data) =>
+patchArticle(5588, { title: "바뀐 제목", content: "바뀐 내용" }).then((data) =>
   console.log("수정된 글", data),
 ); //게시글 수정
 
-deleteArticle(2990).then(() => console.log("삭제됨")); //게시글 삭제
+deleteArticle(2990).then(() => console.log("글 삭제됨")); //게시글 삭제
 
-getProductList(1, 10, "").then((data) => console.log("상품목록", data)); //상품 목록 불러오기
+const ProductListGet = async () => {
+  const data = await getProductList(1, 10, "");
+  console.log("상품목록", data);
+};
 
-getProduct(2998).then((data) => console.log("상품", data)); //특정 상품 불러오기
+ProductListGet();
 
-createProduct(
-  "상품 이름",
-  "string",
-  0,
-  ["전자제품"],
-  ["https://example.com/..."],
-).then((data) => console.log("생성상품", data)); //상품 생성
+const ProductGet = async () => {
+  const data = await getProduct(2998);
+  console.log("상품", data);
+};
 
-patchProduct(2993, { name: "바뀐 상품명", price: 5000 }).then((data) =>
-  console.log("수정된 상품", data),
-); //상품 수정
+ProductGet();
 
-deleteProduct(2997).then(() => console.log("상품 삭제됨")); //상품 삭제
+const ProductCreate = async () => {
+  const data = await createProduct(
+    "에어프라이어",
+    "소형 가정용 에어프라이어",
+    30000,
+    ["전자제품"],
+    ["https://example.com/..."],
+  );
+  console.log("생성상품", data);
+};
+
+ProductCreate();
+
+const ProductsPatch = async () => {
+  const data = await patchProduct(2993, { name: "드라이기", price: 5000 });
+  console.log("수정된 상품", data);
+};
+
+ProductsPatch();
+
+const ProductDelete = async () => {
+  await deleteProduct(2997);
+  console.log("상품 삭제됨");
+};
+
+ProductDelete();
