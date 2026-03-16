912b45002d6b40d966533d2800d75adee897bdc1
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-03-16 10:25:30 +0900

    feat: 스프린트 미션 3

---
+++ static/js/api/ProductService.js
@@ -0,0 +1,85 @@
+const API_URL = "https://panda-market-api-crud.vercel.app";
+
+export async function getProductList(
+  page = 1, // 불러올 페이지 default 1
+  pageSize = 10, // 불러올 상품 수 default 10
+  orderBy = "recent", // 불러올 정렬방식 default recent (최신순)
+  keyword = "", // 검색 키워드 default ''
+) {
+  try {
+    const res = await fetch(
+      `${API_URL}/products?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}${keyword ? "&keyword=" + keyword : ""}`,
+    );
+    const products = await res.json();
+
+    return products;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getProduct(pid) {
+  try {
+    const res = await fetch(`${API_URL}/products/${pid}`);
+    const product = await res.json();
+
+    return product;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createProduct(product) {
+  try {
+    const { name, description, price, tags, images } = product;
+
+    const res = await fetch(`${API_URL}/products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        price,
+        tags,
+        images,
+      }),
+    });
+    const createdProduct = await res.json();
+
+    return createdProduct;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function patchProduct(pid, data) {
+  try {
+    const res = await fetch(`${API_URL}/products/${pid}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const patchedProduct = await res.json();
+
+    return patchedProduct;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteProduct(pid) {
+  try {
+    const res = await fetch(`${API_URL}/products/${pid}`, {
+      method: "DELETE",
+    });
+    const deletedProduct = await res.json();
+
+    return deletedProduct;
+  } catch (error) {
+    console.error(error);
+  }
+}
