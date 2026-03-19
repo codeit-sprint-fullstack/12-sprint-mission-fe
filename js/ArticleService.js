8d58b2ccfa29ad0698553de2f27c1a0dcd03042f
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-19 15:07:24 +0900

    feat: ArticleService 공통 request 함수 구현

---
+++ js/ArticleService.js
@@ -0,0 +1,43 @@
+const BASE_URL = "https://panda-market-api-crud.vercel.app";
+const RESOURCE = "/articles";
+
+function request(path, options = {}) {
+  const url = `${BASE_URL}${RESOURCE}${path}`;
+
+  const config = {
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+    },
+  };
+
+  if (options.body && !config.headers["Content-Type"]) {
+    config.headers["Content-Type"] = "application/json";
+  }
+
+  return fetch(url, config)
+    .then((res) => {
+      return res
+        .json()
+        .catch(() => null)
+        .then((data) => {
+          if (!res.ok) {
+            const error = new Error(
+              `HTTP ${res.status} - ${data?.message || "Unknown error"}`,
+            );
+            error.status = res.status;
+            error.data = data;
+            throw error;
+          }
+          return data;
+        });
+    })
+    .catch((err) => {
+      console.error(
+        `[Article API ERROR] ${options.method || "GET"} ${path}`,
+        err,
+      );
+
+      throw err;
+    });
+}
