0818f4d4000e7f222c4d5cdef9e31f311e3e4a0e
wkdalswn11 <wkdalswn194@gamil.com>
2026-03-19 13:43:54 +0900

    feat: articleService.js 생성 및 작성 , main.js 생성

---
+++ js/ArticleService.js
@@ -0,0 +1,64 @@
+import { BASE_URL } from "./main";
+
+export const getArticleList = async (page, pageSize, keyword) => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/articles?page=${page}&pageSize=${pageSize}&keyword=${keyword}`,
+    );
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getArticle = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/articles/${id}`);
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createArticle = async (title, content, image) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content, image }),
+    });
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const patchArticle = async (id, data) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteArticle = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
