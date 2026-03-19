dfba81a8ac29701becb06ada619600cc12809c32
hogu-giriboy <choo6078@kakao.com>
2026-03-19 20:25:16 +0900

    feat: main.js, ProductService.js, Article.js 파일 작성

--- ProductService.js
+++ ProductService.js
@@ -1,5 +1,100 @@
-getProductList();
-getProduct();
-createProduct();
-patchProduct();
-deleteProduct();
+const PRODUCTS = "https://panda-market-api-crud.vercel.app/products";
+
+export async function getProductList(page, pageSize, keyword) {
+  try {
+    const res = await fetch(
+      `${PRODUCTS}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`,
+    );
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function getProduct(productId) {
+  try {
+    const res = await fetch(`${PRODUCTS}/${productId}`);
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function createProduct(name, description, price, tags, images) {
+  try {
+    const res = await fetch(PRODUCTS, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        price,
+        tags,
+        images,
+      }),
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function patchProduct(
+  productId,
+  name,
+  description,
+  price,
+  tags,
+  images,
+) {
+  try {
+    const res = await fetch(`${PRODUCTS}/${productId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        price,
+        tags,
+        images,
+      }),
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function deleteProduct(productId) {
+  try {
+    const res = await fetch(`${PRODUCTS}/${productId}`, {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+}
---
+++ main.js
@@ -0,0 +1,81 @@
+import {
+  getArticle,
+  getArticleList,
+  createArticle,
+  patchArticle,
+  deleteArticle,
+} from "./ArticleService.js";
+
+import {
+  getProduct,
+  getProductList,
+  createProduct,
+  patchProduct,
+  deleteProduct,
+} from "./ProductService.js";
+
+async function main() {
+  try {
+    // Article
+    const articleListData = await getArticleList(1, 10, "");
+    console.log(articleListData);
+
+    const firstArticleId = articleListData.list[0].id;
+    const article = await getArticle(firstArticleId);
+    console.log(article);
+
+    const createdArticle = await createArticle(
+      "test title",
+      "test content",
+      "https://example.com",
+    );
+    console.log(createdArticle);
+
+    const newArticleId = createdArticle.id;
+    const patchedArticle = await patchArticle(
+      newArticleId,
+      "test title patch",
+      "text content patch",
+      "https://example.com",
+    );
+    console.log(patchedArticle);
+
+    const deletedArticle = await deleteArticle(newArticleId);
+    console.log(deletedArticle);
+
+    // Product
+    const productListData = await getProductList(1, 10, "");
+    console.log(productListData);
+
+    const firstProductId = productListData.list[0].id;
+    const product = await getProduct(firstProductId);
+    console.log(product);
+
+    const createdProduct = await createProduct(
+      "test name",
+      "test destruction",
+      1000,
+      ["text", "test"],
+      ["https://example.com"],
+    );
+    console.log(createdProduct);
+
+    const newProductId = createdProduct.id;
+    const patchedProduct = await patchProduct(
+      newProductId,
+      "test name patch",
+      "test destruction patch",
+      2000,
+      ["test", "patch"],
+      ["https://example.com"],
+    );
+    console.log(patchedProduct);
+
+    const deletedProduct = await deleteProduct(newProductId);
+    console.log(deletedProduct);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
