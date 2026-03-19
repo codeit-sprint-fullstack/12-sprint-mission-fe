db30c6c5c63437992127dd35c7b0cbd89f7b6c2a
narin116 <aasdff1010@gmail.com>
2026-03-19 16:22:20 +0900

    feat: products, articles api구현 완료

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//DOM설정
+//이벤트 설정
+//CRUD 함수 구현
+
+import {
+  getArticleList,
+  getArticle,
+  createArticle,
+  patchArticle,
+  deleteArticle,
+} from "./api/ArticleService.js";
+import {
+  getProductList,
+  getProduct,
+  createProduct,
+  patchProduct,
+  deleteProduct,
+} from "./api/ProductService.js";
