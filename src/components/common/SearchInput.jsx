9f52fbde1fafc401b41478e2be25f96e71cd8125
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-23 18:25:43 +0900

    feat: 검색 input 컴포넌트 구현

---
+++ src/components/common/SearchInput.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import searchIcon from "./ic-search.svg";
+import styles from "./SearchInput.module.css";
+
+export const SearchInput = () => {
+  return (
+    <div className={styles.searchBox}>
+      <img className={styles.icon} src={searchIcon} alt="검색" />
+      <input
+        className={`${styles.input} text-lg-regular`}
+        type="text"
+        name="search"
+        placeholder="검색할 상품을 입력해주세요"
+      />
+    </div>
+  );
+};
