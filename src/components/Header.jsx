005831ec4909176e3620a081097e8f912e18509e
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-23 15:28:17 +0900

    feat: Header 컴포넌트 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logo from "../assets/logo-sm.svg";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={`container ${styles.inner}`}>
+        <div className={`${styles.brandArea}`}>
+          <h1>
+            <a href="/">
+              <img src={logo} alt="판다마켓" />
+            </a>
+          </h1>
+
+          <nav>
+            <ul className={styles.menu}>
+              <li className={`text-2lg-bold ${styles.menuItem}`}>자유게시판</li>
+              <li className={`text-2lg-bold ${styles.menuItem}`}>중고마켓</li>
+            </ul>
+          </nav>
+        </div>
+        <a
+          className={`btn-base text-lg-semibold ${styles.loginBtn}`}
+          href="./login.html"
+        >
+          로그인
+        </a>
+      </div>
+    </header>
+  );
 };
