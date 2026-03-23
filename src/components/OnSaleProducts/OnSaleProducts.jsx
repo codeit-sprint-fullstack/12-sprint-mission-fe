51ee52b0f9c2ace290ec230950a1801c0413c06b
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-23 20:12:03 +0900

    feat: 판매 중인 상품 API 연동 및 데이터 렌더링 구현

--- src/components/OnSaleProducts/OnSaleProducts.jsx
+++ src/components/OnSaleProducts/OnSaleProducts.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./OnSaleProducts.module.css";
+import { getProducts } from "../../api/productsApi";
 import { OnSaleProductHeader } from "./OnSaleProductHeader";
 import { OnSaleProductList } from "./OnSaleProductList";
 
 export const OnSaleProducts = () => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data.list);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <section className={styles.section}>
