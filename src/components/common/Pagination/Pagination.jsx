fbe3f06b08b4fa1e2d454a3fd72750c46451d849
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-24 01:45:14 +0900

    feat: 페이지네이션 접근성 개선(ul/li 구조 및 aria-label, aria-current 적용)

--- src/components/common/Pagination/Pagination.jsx
+++ src/components/common/Pagination/Pagination.jsx
@@ -7,11 +7,12 @@ export const Pagination = ({ page, onPageChange, totalPages }) => {
   const endPage = Math.min(startPage + GROUP_SIZE - 1, totalPages);
 
   return (
-    <div className={styles.pagination}>
+    <nav className={styles.pagination}>
       <button
         className={`${styles.circle} ${styles.pageBtn} ${styles.prev}`}
         disabled={page === 1}
         onClick={() => onPageChange(page - 1)}
+        aria-label="이전 페이지"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -33,20 +34,28 @@ export const Pagination = ({ page, onPageChange, totalPages }) => {
       {Array.from(
         { length: endPage - startPage + 1 },
         (_, i) => startPage + i,
-      ).map((num) => (
-        <button
-          key={num}
-          className={`${styles.circle} ${page === num ? styles.currentPage : styles.link}`}
-          onClick={() => onPageChange(num)}
-        >
-          {num}
-        </button>
-      ))}
+      ).map((num) => {
+        const isCurrent = page === num;
+
+        return (
+          <li key={num} className={styles.pageItem}>
+            <button
+              key={num}
+              className={`${styles.circle} ${isCurrent ? styles.currentPage : styles.link}`}
+              onClick={() => onPageChange(num)}
+              aria-current={isCurrent ? "page" : undefined}
+            >
+              {num}
+            </button>
+          </li>
+        );
+      })}
 
       <button
         className={`${styles.circle} ${styles.pageBtn} ${styles.next}`}
         disabled={page === totalPages}
         onClick={() => onPageChange(page + 1)}
+        aria-label="다음 페이지"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -64,6 +73,6 @@ export const Pagination = ({ page, onPageChange, totalPages }) => {
           />
         </svg>
       </button>
-    </div>
+    </nav>
   );
 };
