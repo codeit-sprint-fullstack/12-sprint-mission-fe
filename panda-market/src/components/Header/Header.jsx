30280710c01f6606d50029ba37cc90d0d9f27391
hogu-giriboy <choo6078@kakao.com>
2026-03-23 18:22:34 +0900

    feat: Header.jsx 작업 중

--- panda-market/src/components/Header/Header.jsx
+++ panda-market/src/components/Header/Header.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h2>판다마켓 헤더</h2>
+      <a href="/">
+        <div class="logo flex-center">
+          <img class="logo-panda" src="판다 얼굴.png" alt="판다마켓 로고" />
+          <span class="logo-name">판다마켓</span>
+        </div>
+      </a>
     </header>
   );
 }
