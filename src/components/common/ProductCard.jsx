1d518a83a30d9c8b24345ef7b4b0bd54605c0d92
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-23 20:09:36 +0900

    feat: 상품 카드에서 기본 이미지 대체 로직 추가

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import heart from "./ic_heart.svg";
+import defaultImg from "./default-img.svg";
 import styles from "./ProductCard.module.css";
 
 export const ProductCard = ({ item }) => {
+  const thumbnailUrl = item.images[0] || defaultImg;
+
   return (
     <li className={styles.card}>
-      <img className={styles.thumbnail} src={item.images[0]} alt={item.name} />
+      <img className={styles.thumbnail} src={thumbnailUrl} alt={item.name} />
 
       <div className={styles.content}>
         <h3 className={`text-md-medium`}>{item.name}</h3>
