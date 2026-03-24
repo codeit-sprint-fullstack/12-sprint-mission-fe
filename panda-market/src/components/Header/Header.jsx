b1a1e4bde2a94bd61a2fda2d5986bb0185c5621c
hogu-giriboy <choo6078@kakao.com>
2026-03-24 13:32:11 +0900

    feat: Header.jsx clear

--- panda-market/src/components/Header/Header.jsx
+++ panda-market/src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import logo from "../../assets/header/ic_panda_market_logo.png";
+import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <header>
+    <header className={styles.head}>
       <a href="/">
-        <div class="logo flex-center">
-          <img class="logo-panda" src="판다 얼굴.png" alt="판다마켓 로고" />
-          <span class="logo-name">판다마켓</span>
+        <div className={`${styles.logo} ${styles.flexCenter}`}>
+          <img className={styles.logoPanda} src={logo} alt="판다마켓 로고" />
+          <span className={styles.logoName}>판다마켓</span>
         </div>
+      </a>
+      <a href="login.html" className={`${styles.btn} ${styles.btnLogin}`}>
+        로그인
       </a>
     </header>
   );
