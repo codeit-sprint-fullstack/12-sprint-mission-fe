4df3da741f872b68e0007baae3d3d8c9d43957f1
hogu-giriboy <choo6078@kakao.com>
2026-03-26 02:08:07 +0900

    feat: ProductList.jsx, BestProducts.jsx grid 수정

--- panda-market/src/components/ProductList/ProductList.jsx
+++ panda-market/src/components/ProductList/ProductList.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import styles from "./ProductList.module.css";
 
 function ProductList() {
-  return <div>ProductList</div>;
+  return (
+    <>
+      <section className={styles.ProductList}>
+        <div>
+          <h2 className={styles.listH2}>판매중인 상품</h2>
+        </div>
+        <div className={styles.listGrid}>
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+          <ProductCard title="로봇 청소기" price={1500000} heartCount={240} />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ProductList;
