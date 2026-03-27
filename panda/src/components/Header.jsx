477e0ac957951b8f253be350208ff0d0d624e982
apple <apple@apples-MacBook-Air.local>
2026-03-27 19:07:07 +0900

    feat: Footer, Header 영역 추가 및 컴포넌트 분리

--- panda/src/components/Header.jsx
+++ panda/src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div>
+          <Link className="logo" to="/" />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/community">자유게시판</Link>
+              </li>
+              <li>
+                <Link to="/items">중고마켓</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Link to="/login">로그인</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
