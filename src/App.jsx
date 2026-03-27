496821ea03545542a7b017ecdc9028fd1af5f432
wkdalswn11 <wkdalswn194@gamil.com>
2026-03-27 19:08:47 +0900

    feat:에러수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import ProductList from "./pages/ProductList";
+import ProductList from "./pages/productList";
 
 import { Route, Routes } from "react-router-dom";
 
