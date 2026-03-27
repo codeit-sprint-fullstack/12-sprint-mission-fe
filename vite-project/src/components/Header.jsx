db59c52f77ef5e955bf33e68a64e4c4f59239e51
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-03-27 10:45:34 +0900

    feat: 헤더 nac 추가

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -6,9 +6,13 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <a href="/">
-          <img className="header-logo" src={logo} alt="로고" />
-        </a>
+        <div className="header-inner">
+          <a href="/">
+            <img className="header-logo" src={logo} alt="로고" />
+          </a>
+          <nav className="nav-button">자유게시판</nav>
+          <nav className="nav-button">중고마켓</nav>
+        </div>
         <nav className="header-navigation">
           <a href="/" className="header-login-button">
             로그인
