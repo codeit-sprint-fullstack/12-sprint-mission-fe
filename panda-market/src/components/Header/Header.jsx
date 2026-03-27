549535e566eeab25355b923c62fbac24a9288854
hogu-giriboy <choo6078@kakao.com>
2026-03-27 10:42:07 +0900

    feat: Hear.jsx style clear

--- panda-market/src/components/Header/Header.jsx
+++ panda-market/src/components/Header/Header.jsx
@@ -5,13 +5,21 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.head}>
-      <a href="/">
-        <div className={`${styles.logo} ${styles.flexCenter}`}>
+      <div className={styles.navWrap}>
+        <a href="/" className={styles.logo}>
           <img className={styles.logoPanda} src={logo} alt="판다마켓 로고" />
           <span className={styles.logoName}>판다마켓</span>
+        </a>
+        <div style={{ display: "inline-flex" }}>
+          <a href="/" className={styles.navLink}>
+            자유게시판
+          </a>
+          <a href="/" className={styles.navLink}>
+            중고마켓
+          </a>
         </div>
-      </a>
-      <a href="login.html" className={`${styles.btn} ${styles.btnLogin}`}>
+      </div>
+      <a href="/" className={`${styles.btn} ${styles.btnLogin}`}>
         로그인
       </a>
     </header>
