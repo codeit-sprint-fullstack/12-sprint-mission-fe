c3e447422f4b1b0b22ba40db11e6dce10b4de758
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-28 23:28:53 +0900

    feat: 상품 목록 페이지 반응형 레이아웃 추가

--- src/components/OnSaleProducts/OnSaleProductHeader.jsx
+++ src/components/OnSaleProducts/OnSaleProductHeader.jsx
@@ -12,10 +12,18 @@ export const OnSaleProductHeader = ({
   return (
     <div className={styles.container}>
       <h2 className={`${styles.title} text-xl-bold`}>판매 중인 상품</h2>
-
+      <a
+        className={`btn-base text-lg-semibold ${styles.btn} show-mobile`}
+        href="/"
+      >
+        상품 등록하기
+      </a>
       <div className={styles.controls}>
         <SearchInput value={keyword} onChange={onKeywordChange} />
-        <a className={`btn-base text-lg-semibold ${styles.btn}`} href="/">
+        <a
+          className={`btn-base text-lg-semibold ${styles.btn} hide-mobile`}
+          href="/"
+        >
           상품 등록하기
         </a>
         <SortSelect value={sortBy} onChange={onSortChange} />
