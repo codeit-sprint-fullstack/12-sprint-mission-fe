c439ae69e2159fc1bd4cfe708b0a039d0fd6adb5
gptjd0204 <gptjd0204@naver.com>
2026-03-30 18:03:53 +0900

    feat: 상품 등록 페이지 목업 생성

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Registration = () => {
-  return <div>Registration</div>;
+  return (
+    <main>
+      <form>
+        <section>
+          <h1>상품 등록하기</h1>
+          <button>등록</button>
+        </section>
+        <section>
+          <h2>상품명</h2>
+          <input />
+        </section>
+        <section>
+          <h2>상품 소개</h2>
+          <input />
+        </section>
+        <section>
+          <h2>판매가격</h2>
+          <input />
+        </section>
+        <section>
+          <h2>태그</h2>
+          <input />
+        </section>
+      </form>
+    </main>
+  );
 };
 
 export default Registration;
