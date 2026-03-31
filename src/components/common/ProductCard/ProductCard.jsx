f0bcbb7daf53ca8977bd7527588e975f2054a830
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-31 13:41:09 +0900

    refactor(mentor): 상품 이미지 로딩 지연 시 썸네일 영역에 스켈레톤 UI 적용

--- src/components/common/ProductCard/ProductCard.jsx
+++ src/components/common/ProductCard/ProductCard.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import heart from "./ic-heart.svg";
 import defaultImg from "./default-img.svg";
 import styles from "./ProductCard.module.css";
 
 export const ProductCard = ({ type, item }) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
   const thumbnailUrl = item.images[0] || defaultImg;
 
   return (
     <li className={styles.card}>
+      {!imgLoaded && (
+        <div
+          className={`${styles.thumbnail} ${styles[type]} ${styles.imgSkeleton}`}
+        />
+      )}
       <img
         className={`${styles.thumbnail} ${styles[type]}`}
         src={thumbnailUrl}
         alt={item.name}
+        onLoad={() => setImgLoaded(true)}
         onError={(e) => {
           e.target.src = defaultImg;
+          setImgLoaded(true);
         }}
+        style={{ display: imgLoaded ? "block" : "none" }}
       />
 
       <div className={styles.content}>
