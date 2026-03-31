ee32617ff79ac9d0c497711bb663e8adcf1e3f5d
gptjd0204 <gptjd0204@naver.com>
2026-03-31 10:15:53 +0900

    feat: 상품 등록 페이지 스타일 추가

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,29 +1,50 @@
 import React from "react";
+import styles from "../styles/Registration.module.css";
 
 const Registration = () => {
   return (
-    <main>
-      <form>
-        <section>
-          <h1>상품 등록하기</h1>
-          <button>등록</button>
-        </section>
-        <section>
-          <h2>상품명</h2>
-          <input />
-        </section>
-        <section>
-          <h2>상품 소개</h2>
-          <input />
-        </section>
-        <section>
-          <h2>판매가격</h2>
-          <input />
-        </section>
-        <section>
-          <h2>태그</h2>
-          <input />
-        </section>
+    <main className={styles.wrapper}>
+      <form className={styles.registrationContainer}>
+        <div className={styles.btnContainer}>
+          <h1 className="text-xl bold">상품 등록하기</h1>
+          <button type="submit" className={`${styles.addBtn} text-lg semibold`}>
+            등록
+          </button>
+        </div>
+        <div className={styles.inputContainer}>
+          <section>
+            <h2 className="text-2lg bold">상품명</h2>
+            <input
+              type="text"
+              className={`${styles.nameInput} text-lg regular`}
+              placeholder="상품명을 입력해주세요"
+            />
+          </section>
+          <section>
+            <h2 className="text-2lg bold">상품 소개</h2>
+            <textarea
+              type="text"
+              className={`${styles.descriptionInput} text-lg regular`}
+              placeholder="상품 소개를 입력해주세요"
+            />
+          </section>
+          <section>
+            <h2 className="text-2lg bold">판매가격</h2>
+            <input
+              type="text"
+              className={`${styles.priceInput} text-lg regular`}
+              placeholder="상품 가격을 입력해주세요"
+            />
+          </section>
+          <section>
+            <h2 className="text-2lg bold">태그</h2>
+            <input
+              type="text"
+              className={`${styles.tagInput} text-lg regular`}
+              placeholder="태그를 입력해주세요"
+            />
+          </section>
+        </div>
       </form>
     </main>
   );
