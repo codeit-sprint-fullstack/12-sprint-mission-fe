744239036c792c07fc96ca792b195d9f998e9e6c
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-05 05:03:18 +0900

    style: pagination css module화

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,88 @@
+import paginationStyles from "./Pagination.module.css";
+
+const PAGE_GROUP = 5;
+
+const Pagination = ({ page, totalPages, onNext, onPrev, onChangePage }) => {
+  const currentGroup = Math.floor((page - 1) / PAGE_GROUP);
+  const groupStartPage = currentGroup * PAGE_GROUP + 1;
+  const groupEndPage = Math.min(groupStartPage + PAGE_GROUP - 1, totalPages);
+  const viewPages = [];
+  for (let i = groupStartPage; i <= groupEndPage; i++) {
+    viewPages.push(i);
+  }
+
+  return (
+    <div className={`${paginationStyles.pagination}`}>
+      <button
+        onClick={onPrev}
+        disabled={page === 1}
+        className={`${paginationStyles.page} ${paginationStyles.prev}`}
+      >
+        <span className="sr-only">이전</span>
+      </button>
+
+      {viewPages.map((p) => (
+        <button
+          key={p}
+          onClick={() => {
+            onChangePage(p);
+          }}
+          className={`${paginationStyles.page} ${page === p && paginationStyles.current}`}
+        >
+          {p}
+        </button>
+      ))}
+
+      {/* <button
+        onClick={() => {
+          onChangePage(1);
+        }}
+        className={`btn-page ${page === 1 ? "current" : ""}`}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          onChangePage(2);
+        }}
+        className={`btn-page ${page === 2 ? "current" : ""}`}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          onChangePage(3);
+        }}
+        className={`btn-page ${page === 3 ? "current" : ""}`}
+      >
+        3
+      </button>
+      <button
+        onClick={() => {
+          onChangePage(4);
+        }}
+        className={`btn-page ${page === 4 ? "current" : ""}`}
+      >
+        4
+      </button>
+      <button
+        onClick={() => {
+          onChangePage(5);
+        }}
+        className={`btn-page ${page === 5 ? "current" : ""}`}
+      >
+        5
+      </button> */}
+
+      <button
+        onClick={onNext}
+        disabled={page === totalPages}
+        className={`${paginationStyles.page} ${paginationStyles.next}`}
+      >
+        <span className="sr-only">다음</span>
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/routes/Items/Items.jsx
+++ src/routes/Items/Items.jsx
@@ -1,11 +1,12 @@
 import ProductWrap from "../../components/ProductWrap";
 import SearchForm from "../../components/SearchForm";
 import ProductList from "../../components/ProductList";
-import Pagination from "../../components/Pagination";
+import Pagination from "../../components/Pagination/Pagination";
 import { useEffect, useState } from "react";
 import usePageSize from "../../hooks/usePageSize";
 import { getProductList } from "../../api/products";
 import { Link } from "react-router-dom";
+import Sorting from "../../components/Sorting";
 
 const Items = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,7 +17,8 @@ const Items = () => {
   const [debouncedKeyword, setDebouncedKeyword] = useState("");
   const totalLimit = usePageSize("total");
   const totalPages = Math.ceil(totalCount / totalLimit);
-
+  const [sorting, setSorting] = useState("recent");
+  const [isSortOpen, setIsSortOpen] = useState(false);
   // console.log("total p s => ", totalLimit, "/// best p s => ", bestLimit);
 
   useEffect(() => {
@@ -44,6 +46,11 @@ const Items = () => {
     setPage(1);
   };
 
+  const handleChangeSorting = (sort) => {
+    setSorting(sort);
+    // setIsSortOpen((prev) => !prev);
+  };
+
   const handlePrevPage = () => {
     setPage((prev) => prev - 1);
   };
@@ -62,11 +69,22 @@ const Items = () => {
           <Link to={`/registration`} className="btn-primary btn-sm">
             상품 등록하기
           </Link>
+
+          <Sorting sorting={sorting} isSortOpen={isSortOpen} />
         </>
       }
     >
       {isLoading ? (
-        <p>상품을 불러오는 중...</p>
+        <p
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingBlock: 30,
+          }}
+        >
+          상품을 불러오는 중...
+        </p>
       ) : (
         <ProductList lists={products} type="total" />
       )}
