ccf2b939ad76fefd5d3731a011d579954f041e9c
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-06 17:30:00 +0900

    refactor: common 폴더를 layout 폴더로 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
