1f1c5a87a6fe702a72c33eb64740a7b4b7eea744
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-07 10:07:38 +0900

    feat: 게시글 상세 페이지에서 댓글 0개일 때 빈 상태 UI 추가

--- src/app/community/[id]/_components/CommentList.js
+++ src/app/community/[id]/_components/CommentList.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import { getArticleComments } from "@/lib/api/posts";
 import CommentCard from "./CommentCard";
 
@@ -18,9 +19,25 @@ export default function CommentList({ postId }) {
 
   return (
     <div className="flex flex-col gap-4 md:gap-6 mb-10 md:mb-14 lg:mb-16">
-      {comments.map((comment) => (
-        <CommentCard key={comment.id} comment={comment} />
-      ))}
+      {comments.length == 0 ? (
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/icons/ic-empty-comment.svg"
+            width={140}
+            height={140}
+            alt=""
+            aria-hidden="true"
+          />
+          <p className="text-lg-regular text-gray-400 text-center">
+            아직 댓글이 없어요,
+            <br /> 지금 댓글을 달아보세요!
+          </p>
+        </div>
+      ) : (
+        comments.map((comment) => (
+          <CommentCard key={comment.id} comment={comment} />
+        ))
+      )}
     </div>
   );
 }
