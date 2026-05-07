cc451293357ea35aafcb1d4f0fb52b1fbfab6a82
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-07 11:52:31 +0900

    feat: Button 컴포넌트 secondary variant 추가

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -5,19 +5,28 @@ export default function Button({
   href,
   onClick,
   disabled,
+  variant = "primary",
   className = "",
 }) {
   const baseStyles =
     "inline-flex items-center justify-center gap-[0.625rem] whitespace-nowrap transition-all leading-none";
 
-  const colorStyles = `
-    bg-primary text-white 
-    hover:bg-primary-dark 
-    active:bg-primary-darker 
-    disabled:bg-gray-400 disabled:cursor-not-allowed
-  `;
+  const variantStyles = {
+    primary: `
+      bg-primary text-white
+      hover:bg-primary-dark
+      active:bg-primary-darker
+      disabled:bg-gray-400 disabled:cursor-not-allowed
+    `,
 
-  const combinedClassName = `${baseStyles} ${colorStyles} ${className}`;
+    secondary: `
+      bg-white text-gray-500 
+      hover:bg-gray-100
+      active:bg-gray-200
+    `,
+  };
+
+  const combinedClassName = `${baseStyles} ${variantStyles[variant]} ${className}`;
 
   if (href) {
     return (
