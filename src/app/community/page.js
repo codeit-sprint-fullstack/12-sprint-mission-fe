21c230b60cfc853e37ed1f2d774973dc6ea14ad8
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-10 23:59:00 +0900

    feat: BestPostSection에 Suspense 및 Skeleton UI 적용

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import Button from "@/components/ui/Button";
 import BestPostSection from "./_components/BestPostSection";
+import BestPostSectionSkeleton from "./_components/BestPostSectionSkeleton";
 import PostSection from "./_components/PostSection";
 
 export const metadata = {
@@ -11,7 +13,9 @@ export default function CommunityPage() {
     <div className="flex flex-col gap-10">
       <section className="flex flex-col gap-6 mb-6 lg:mb-10">
         <h2 className="text-xl-bold">베스트 게시글</h2>
-        <BestPostSection />
+        <Suspense fallback={<BestPostSectionSkeleton />}>
+          <BestPostSection />
+        </Suspense>
       </section>
       <section>
         <div className="flex justify-between items-center w-full mb-4 md:mb-12 lg:mb-6">
