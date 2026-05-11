2cfda3144676f033aff1043428425367e34131e8
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-11 18:18:28 +0900

    feat: 커뮤니티 페이지에 error ui 추가

--- src/app/community/[id]/error.jsx
+++ src/app/community/[id]/error.jsx
@@ -7,7 +7,7 @@ export default function Error({ reset }) {
   return (
     <div className="flex flex-col items-center justify-center gap-6 py-40">
       <p className="text-xl-bold text-gray-800">
-        데이터를 불러오는 중 문제가 발생했습니다.
+        게시글을 불러오는 중 문제가 발생했습니다.
       </p>
       <div className="flex flex-col gap-3">
         <Button
---
+++ src/app/community/error.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Button from "@/components/ui/Button";
+
+export default function Error({ reset }) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 py-40">
+      <p className="text-xl-bold text-gray-800">
+        게시글 목록을 불러오는 중 오류가 발생했습니다.
+      </p>
+      <Button
+        variant="secondary"
+        size="lg"
+        rounded="full"
+        className="w-[15rem]"
+        onClick={() => reset()}
+      >
+        <span className="text-2lg-semibold">다시 시도</span>
+      </Button>
+    </div>
+  );
+}
