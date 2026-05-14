dd002358247e9a1cff73ad260c2b423363405459
gptjd0204 <gptjd0204@naver.com>
2026-05-14 17:35:14 +0900

    feat: 게시글 검색 기능 구현

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import React from "react";
-import icSearch from "../../../public/icons/ic_search.png";
-import icSortBtn from "../../../public/icons/ic_btn_sort.png";
+
 import BestItemList from "./_components/BestItemList";
 import ArticlesList from "./_components/ArticlesList";
 
@@ -21,31 +19,6 @@ const CommunityPage = () => {
             <button className="text-lg font-semibold text-white text-center rounded-lg bg-(--Primary-100) px-[23px] py-[8px]">
               글쓰기
             </button>
-          </div>
-          <div className="flex justify-between gap-2">
-            <div className="relative w-full">
-              <Image
-                src={icSearch}
-                alt="검색 아이콘"
-                className="absolute top-2 left-4"
-              />
-              <input
-                type="text"
-                placeholder="검색할 상품을 입력해주세요"
-                className="w-full h-[42px] pl-11 pr-3 bg-(--Secondary-100) rounded-xl text-lg"
-              />
-            </div>
-            <select className="hidden w-[130px] px-[20px] border-1 border-(--Secondary-200) bg-white rounded-xl md:block">
-              <option>최신순</option>
-              <option>좋아요순</option>
-            </select>
-            <Image
-              src={icSortBtn}
-              alt="정렬 버튼 아이콘"
-              width={42}
-              height={42}
-              className="block shrink-0 md:hidden"
-            />
           </div>
           <ArticlesList />
         </section>
