bf44304d549f33a7b94cab2026b35cdec5cf4712
yooseohyeon <yooseohyeon.02@gmail.com>
2026-07-03 22:56:50 +0900

    refactor: WithLayout TSX로 마이그레이션

---
+++ src/app/(with-layout)/layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+
+type WithLayoutProps = {
+  children: ReactNode;
+};
+
+export default function WithLayout({ children }: WithLayoutProps) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
