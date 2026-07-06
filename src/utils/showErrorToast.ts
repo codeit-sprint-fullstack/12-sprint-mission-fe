5e1ac9420e230df48f26d843e46eea7773ff17a3
yooseohyeon <yooseohyeon.02@gmail.com>
2026-07-06 18:31:57 +0900

    refactor: showErrorToast TS로 마이그레이션

---
+++ src/utils/showErrorToast.ts
@@ -0,0 +1,7 @@
+import { toast } from "react-hot-toast";
+
+import type { ApiError } from "@/types/api";
+
+export const showErrorToast = (err: ApiError | Error, action: string) => {
+  toast.error(err.message || `${action}에 실패했습니다.`);
+};
